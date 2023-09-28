1644299894cc789e8a6379a329db9d99620f3306
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-09-28 14:47:51 +0900

    캐릭터 4방향 이동 애니메이션 추가

--- Lecture07_Linear_Movement/move_character_with_mouse.py
+++ Lecture07_Linear_Movement/move_character_with_mouse.py
@@ -5,8 +5,10 @@ TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
-keroro_left = load_image('keroro_left.png')
-keroro_right = load_image('keroro_right.png')
+keroro_left_down = load_image('keroro_left_down.png')
+keroro_right_down = load_image('keroro_right_down.png')
+keroro_left_up = load_image('keroro_left_up.png')
+keroro_right_up = load_image('keroro_right_up.png')
 hand = load_image('hand_arrow.png')
 
 def handle_events():
@@ -28,20 +30,25 @@ def produce_hand():
 
 def move_keroro(keroro_x, keroro_y, hand_x, hand_y):
     frame = 0
-    for i in range(0, 100 + 1, 4):
+    for i in range(0, 100 + 1, 5):
         clear_canvas()
         TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
         hand.draw(hand_x, hand_y)
         t = i / 100
         x = (1 - t) * keroro_x + t * hand_x
         y = (1 - t) * keroro_y + t * hand_y
-        if hand_x > keroro_x:
-            keroro_right.clip_draw(frame * 250, 0, 250, 345, x, y, 125, 160)
-        elif hand_x < keroro_x:
-            keroro_left.clip_draw(frame * 250, 0, 250, 345, x, y, 125, 160)
+        if hand_x > keroro_x and hand_y > keroro_y:
+            keroro_right_up.clip_draw(frame * 290, 0, 290, 345, x, y, 125, 160)
+        elif hand_x < keroro_x and hand_y > keroro_y:
+            keroro_left_up.clip_draw(frame * 290, 0, 290, 345, x, y, 125, 160)
+        elif hand_x > keroro_x and hand_y < keroro_y:
+            keroro_right_down.clip_draw(frame * 260, 0, 260, 350, x, y, 125, 160)
+        elif hand_x < keroro_x and hand_y < keroro_y:
+            keroro_left_down.clip_draw(frame * 260, 0, 260, 350, x, y, 125, 160)
         update_canvas()
         frame = (frame + 1) % 4
         delay(0.08)
+        handle_events()
 
 running = True
 keroro_x, keroro_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
